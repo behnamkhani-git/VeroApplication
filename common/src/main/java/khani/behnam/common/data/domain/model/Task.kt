29fbc863d94d3c0eedfd510b777fd87d0838906e
Beh <behnamkhani@hotmail.com>
2024-08-07 19:48:23 +0330

    Add domain model

---
+++ common/src/main/java/khani/behnam/common/data/domain/model/Task.kt
@@ -0,0 +1,16 @@
+package khani.behnam.common.data.domain.model
+
+data class Task (
+    val task: String,
+    val title: String,
+    val description: String,
+    val sort: String,
+    val wageType: String,
+    val businessUnitKey: String?,
+    val businessUnit: String,
+    val parentTaskId: String,
+    val preplanningBoardQuickSelect: String?,
+    val colorCode: String,
+    val workingTime: String?,
+    val isAvailableInTimeTrackingKioskMode: Boolean,
+)
