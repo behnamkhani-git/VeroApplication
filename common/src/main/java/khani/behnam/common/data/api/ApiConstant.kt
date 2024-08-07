78ed566827c7a3dedbf87e463c587a9963e33ab4
Beh <behnamkhani@hotmail.com>
2024-08-07 20:16:02 +0330

    Define api literals

---
+++ common/src/main/java/khani/behnam/common/data/api/ApiConstant.kt
@@ -0,0 +1,12 @@
+package khani.behnam.common.data.api
+
+object ApiConstant {
+    const val AUTH_HEADER = "Basic QVBJX0V4cGxvcmVyOjEyMzQ1NmlzQUxhbWVQYXNz"
+    const val CONTENT_TYPE = "application/json"
+    const val USER_NAME = "365"
+    const val PASSWORD = "1"
+
+    const val BASE_URL = "https://api.baubuddy.de"
+    const val LOGIN_ENDPOINT =  "/index.php/login"
+    const val TASK_ENDPOINT = "/dev/index.php/v1/tasks/select"
+}
