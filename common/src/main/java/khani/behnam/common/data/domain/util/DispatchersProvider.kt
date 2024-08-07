bab2b65d4e3d086f06f67f87f342b716c13b1486
Beh <behnamkhani@hotmail.com>
2024-08-07 19:53:42 +0330

    Add dispatcher provider

---
+++ common/src/main/java/khani/behnam/common/data/domain/util/DispatchersProvider.kt
@@ -0,0 +1,8 @@
+package khani.behnam.common.data.domain.util
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+interface DispatchersProvider {
+    fun io(): CoroutineDispatcher = Dispatchers.IO
+}
