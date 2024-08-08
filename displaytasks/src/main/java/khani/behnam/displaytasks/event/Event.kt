8c16a7b3f31ed7701d74e1d19d3456ecd6e37311
Beh <behnamkhani@hotmail.com>
2024-08-08 13:53:53 +0330

    Add Event<T> to handle one-time events

---
+++ displaytasks/src/main/java/khani/behnam/displaytasks/event/Event.kt
@@ -0,0 +1,13 @@
+package khani.behnam.displaytasks.event
+
+data class Event<out T>(private val content: T) {
+    private var hasBeenHandled = false
+    fun getContentIfNotHandled(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+}
