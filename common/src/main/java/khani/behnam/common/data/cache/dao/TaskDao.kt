baed049959026c98e73a318b3afac5d756cd7524
Beh <behnamkhani@hotmail.com>
2024-08-08 19:10:35 +0330

    Search by all the possible indexes

--- common/src/main/java/khani/behnam/common/data/cache/dao/TaskDao.kt
+++ common/src/main/java/khani/behnam/common/data/cache/dao/TaskDao.kt
@@ -5,8 +5,8 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
-import khani.behnam.common.data.cache.model.CachedTask
 import kotlinx.coroutines.flow.Flow
+import khani.behnam.common.data.cache.model.CachedTask
 
 @Dao
 abstract class TaskDao {
@@ -15,9 +15,20 @@ abstract class TaskDao {
     @Query("SELECT * FROM tasks")
     abstract fun getAllTasks(): Flow<List<CachedTask>>
 
-    @Transaction
-    @Query("SELECT * FROM tasks WHERE task LIKE '%' || :searchQuery || '%'")
+    @Query("SELECT * FROM tasks WHERE task LIKE '%' || :searchQuery || '%' " +
+            "OR title LIKE '%' || :searchQuery || '%'" +
+            "OR description LIKE '%' || :searchQuery || '%'" +
+            "OR sort LIKE '%' || :searchQuery || '%'" +
+            "OR wageType LIKE '%' || :searchQuery || '%'" +
+            "OR businessUnitKey LIKE '%' || :searchQuery || '%'" +
+            "OR businessUnit LIKE '%' || :searchQuery || '%'" +
+            "OR parentTaskId LIKE '%' || :searchQuery || '%'" +
+            "OR preplanningBoardQuickSelect LIKE '%' || :searchQuery || '%'" +
+            "OR colorCode LIKE '%' || :searchQuery || '%'" +
+            "OR workingTime LIKE '%' || :searchQuery || '%'" +
+            "OR isAvailableInTimeTrackingKioskMode LIKE '%' || :searchQuery || '%'")
     abstract suspend fun searchTasks(searchQuery: String): List<CachedTask>
+
 
     @Transaction
     @Insert(onConflict = OnConflictStrategy.IGNORE)
