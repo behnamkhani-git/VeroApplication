cb0277515a18ac9605a120edf9086168a6e26345
Beh <behnamkhani@hotmail.com>
2024-08-09 19:14:44 +0330

    Add ResourceUpdateWorker

--- displaytasks/src/main/java/khani/behnam/displaytasks/worker/ResourceUpdateWorker.kt
+++ displaytasks/src/main/java/khani/behnam/displaytasks/worker/ResourceUpdateWorker.kt
@@ -2,21 +2,27 @@ package khani.behnam.displaytasks.worker
 
 import android.content.Context
 import android.util.Log
+import androidx.hilt.work.HiltWorker
+
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import khani.behnam.common.domain.usecase.RequestTasks
 import javax.inject.Inject
 
-class ResourceUpdateWorker @Inject constructor(
-    private val requestTasks: RequestTasks,
-    context: Context,
-    params: WorkerParameters) : CoroutineWorker(context, params) {
+@HiltWorker
+class ResourceUpdateWorker @AssistedInject constructor(
+    @Assisted private val requestTasks: RequestTasks,
+    @Assisted context: Context,
+    @Assisted params: WorkerParameters) : CoroutineWorker(context, params) {
 
     override suspend fun doWork(): Result {
         return try {
             requestTasks()
             Result.success()
         } catch (e: Exception) {
+            Log.e("ResourceUpdateWorker", "Work failed", e)
             e.printStackTrace()
             Result.failure()
         }
